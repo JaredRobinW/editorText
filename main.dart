62a0a52cb0fbde7da83ddaf6f8a49852f95c0410
Robinson Donoso <74510881+JaredRobinW@users.noreply.github.com>
2023-11-14 21:34:43 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:appp1/front/principal.dart';
+
+void main(){
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget{
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const WidPrincipal(
+    );
+  }
+}
+
